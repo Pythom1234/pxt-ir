f3ebd98f2be8a064c7672c71cc4d0af3c62c8228
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-02-14 14:35:03 +0100

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-//% icon="\uf09e" color=#014598 block="IR Reciever" block.loc.cs="IR Příjímač"
+//% icon="\uf09e" color=#014598 block="IR Reciever" block.loc.cs="IR Přijímač"
 namespace IRReciever {
     let irPin: AnalogPin
     //% block="connect IR reciever at pin %pin"
+    //% block.loc.cs="připojit IR přijímač na pin %pin"
     export function connectReciever(pin: AnalogPin):void {
         irPin = pin
     }
@@ -10,7 +11,8 @@ namespace IRReciever {
 //% icon="\uf09e" color=#014598 block="IR Transmitter" block.loc.cs="IR Vysílač"
 namespace IRTransmitter {
     let irPin: AnalogPin
-    //% block="connect IR reciever at pin %pin"
+    //% block="connect IR transmitter at pin %pin"
+    //% block.loc.cs="připojit IR vysílač na pin %pin"
     export function connectTransmitter(pin: AnalogPin): void {
         irPin = pin
     }
