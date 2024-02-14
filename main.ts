dc49179f5246edd9a709f805e60a9525f45775e6
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-02-14 14:22:40 +0100

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-//% icon="\uf09e" color=#014598
-namespace IR_Reciever {
+//% icon="\uf09e" color=#014598 block="IR Reciever"
+namespace IRReciever {
     let irPin: AnalogPin
     //% block="connect IR reciever at pin %pin"
     export function connectReciever(pin: AnalogPin):void {
