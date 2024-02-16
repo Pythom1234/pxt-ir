040a729716525c26a8caa40518d315124ad9fb1c
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-02-16 15:04:48 +0100

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -78,3 +78,316 @@ namespace IRTransmitter {
         transmitBit(BIT_MARK, 100)
     }
 }
+
+
+
+//% color=#014598 icon="\uf09e" block="LEGO Powerfunctions"
+namespace irLegoPowerfunctions {
+    enum PowerFunctionsChannel {
+        //% block="1"
+        One = 0,
+        //% block="2"
+        Two = 1,
+        //% block="3"
+        Three = 2,
+        //% block="4"
+        Four = 3,
+    }
+    enum PowerFunctionsDirection {
+        //% block="left"
+        Left = 1,
+        //% block="right"
+        Right = -1,
+    }
+    enum PowerFunctionsOutput {
+        //% block="red"
+        Red = 0,
+        //% block="blue"
+        Blue = 1,
+    }
+    enum PowerFunctionsMotor {
+        //% block="red | channel 1"
+        Red1 = 0,
+        //% block="red | channel 2"
+        Red2 = 1,
+        //% block="red | channel 3"
+        Red3 = 2,
+        //% block="red | channel 4"
+        Red4 = 3,
+        //% block="blue | channel 1"
+        Blue1 = 4,
+        //% block="blue | channel 2"
+        Blue2 = 5,
+        //% block="blue | channel 3"
+        Blue3 = 6,
+        //% block="blue | channel 4"
+        Blue4 = 7,
+    }
+    enum PowerFunctionsCommand {
+        //% block="float"
+        Float = 0,
+        //% block="forward"
+        Forward = 1,
+        //% block="backward"
+        Backward = 2,
+        //% block="brake"
+        Brake = 3,
+    }
+    interface PowerFunctionsState {
+        irDevice: InfraredDevice;
+        motorDirections: PowerFunctionsDirection[];
+    }
+
+    let state: PowerFunctionsState;
+
+    function getChannel(motor: PowerFunctionsMotor): PowerFunctionsChannel {
+        const MOTOR_TO_CHANNEL = [
+            PowerFunctionsChannel.One,
+            PowerFunctionsChannel.Two,
+            PowerFunctionsChannel.Three,
+            PowerFunctionsChannel.Four,
+            PowerFunctionsChannel.One,
+            PowerFunctionsChannel.Two,
+            PowerFunctionsChannel.Three,
+            PowerFunctionsChannel.Four,
+        ];
+        return MOTOR_TO_CHANNEL[motor];
+    }
+
+    function getOutput(motor: PowerFunctionsMotor): PowerFunctionsOutput {
+        const MOTOR_TO_OUTPUT = [
+            PowerFunctionsOutput.Red,
+            PowerFunctionsOutput.Red,
+            PowerFunctionsOutput.Red,
+            PowerFunctionsOutput.Red,
+            PowerFunctionsOutput.Blue,
+            PowerFunctionsOutput.Blue,
+            PowerFunctionsOutput.Blue,
+            PowerFunctionsOutput.Blue,
+        ];
+        return MOTOR_TO_OUTPUT[motor];
+    }
+
+    function sendSingleOutputCommand(
+        channel: PowerFunctionsChannel,
+        output: PowerFunctionsOutput,
+        speed: number
+    ) {
+        const msg = message.createSingleOutputPwmMessage(channel, output, speed);
+        if (state) {
+            state.irDevice.sendMessage(msg);
+        }
+    }
+
+    //% block="connect IR LED at pin %pin"
+    //% weight=90
+    //% pin.fieldEditor="gridpicker" pin.fieldOptions.columns=4 pin.fieldOptions.tooltips="false"
+    export function connectIrLed(pin: AnalogPin) {
+        state = {
+            irDevice: new InfraredDevice(pin),
+            motorDirections: [
+                PowerFunctionsDirection.Left,
+                PowerFunctionsDirection.Left,
+                PowerFunctionsDirection.Left,
+                PowerFunctionsDirection.Left,
+                PowerFunctionsDirection.Left,
+                PowerFunctionsDirection.Left,
+                PowerFunctionsDirection.Left,
+                PowerFunctionsDirection.Left,
+            ],
+        };
+    }
+
+    //% block="set | motor %motor | to %speed"
+    //% speed.min=-7 speed.max=7
+    //% weight=80
+    //% motor.fieldEditor="gridpicker" motor.fieldOptions.columns=4 motor.fieldOptions.tooltips="false"
+    export function setSpeed(motor: PowerFunctionsMotor, speed: number) {
+        speed = Math.max(-7, Math.min(7, speed));
+        if (state) {
+            sendSingleOutputCommand(
+                getChannel(motor),
+                getOutput(motor),
+                speed * state.motorDirections[motor]
+            );
+        }
+    }
+
+    //% block="brake| motor %motor"
+    //% weight=75
+    //% motor.fieldEditor="gridpicker" motor.fieldOptions.columns=4 motor.fieldOptions.tooltips="false"
+    export function brake(motor: PowerFunctionsMotor) {
+        setSpeed(motor, 0);
+    }
+
+    //% block="float | motor %motor | to stop"
+    //% weight=70
+    //% motor.fieldEditor="gridpicker" motor.fieldOptions.columns=4 motor.fieldOptions.tooltips="false"
+    export function float(motor: PowerFunctionsMotor) {
+        if (state) {
+            sendSingleOutputCommand(getChannel(motor), getOutput(motor), 8);
+        }
+    }
+
+    //% block="set direction | of motor %motor | to %direction"
+    //% weight=20
+    //% motor.fieldEditor="gridpicker" motor.fieldOptions.columns=4 motor.fieldOptions.tooltips="false"
+    export function setMotorDirection(
+        motor: PowerFunctionsMotor,
+        direction: PowerFunctionsDirection
+    ) {
+        if (state) {
+            state.motorDirections[motor] = direction;
+        }
+    }
+
+    namespace message {
+        function mapValueToPwmElseFloat(value: number): number {
+            switch (value) {
+                case 7:
+                    return 0b0111;
+                case 6:
+                    return 0b0110;
+                case 5:
+                    return 0b0101;
+                case 4:
+                    return 0b0100;
+                case 3:
+                    return 0b0011;
+                case 2:
+                    return 0b0010;
+                case 1:
+                    return 0b0001;
+                case 0:
+                    return 0b1000;
+                case -1:
+                    return 0b1111;
+                case -2:
+                    return 0b1110;
+                case -3:
+                    return 0b1101;
+                case -4:
+                    return 0b1100;
+                case -5:
+                    return 0b1011;
+                case -6:
+                    return 0b1010;
+                case -7:
+                    return 0b1001;
+                default:
+                    return 0b0000;
+            }
+        }
+
+        function createMessageFromNibbles(
+            nibble1: number,
+            nibble2: number,
+            nibble3: number
+        ) {
+            const lrc = 0xf ^ nibble1 ^ nibble2 ^ nibble3;
+            return (nibble1 << 12) | (nibble2 << 8) | (nibble3 << 4) | lrc;
+        }
+
+        export function createSingleOutputPwmMessage(
+            channel: PowerFunctionsChannel,
+            output: PowerFunctionsOutput,
+            value: number
+        ) {
+            const nibble1 = 0b0000 + channel;
+            const nibble2 = 0b0100 + output;
+            const nibble3 = mapValueToPwmElseFloat(value);
+            return createMessageFromNibbles(nibble1, nibble2, nibble3);
+        }
+
+        export function createComboDirectMessage(
+            channel: PowerFunctionsChannel,
+            outputRed: PowerFunctionsCommand,
+            outputBlue: PowerFunctionsCommand
+        ) {
+            const nibble1 = 0b0000 + channel;
+            const nibble2 = 0b0001;
+            const nibble3 = (outputBlue << 2) + outputRed;
+            return createMessageFromNibbles(nibble1, nibble2, nibble3);
+        }
+
+        export function createComboPwmMessage(
+            channel: PowerFunctionsChannel,
+            outputRed: number,
+            outputBlue: number
+        ) {
+            const nibble1 = 0b0100 + channel;
+            const nibble2 = mapValueToPwmElseFloat(outputBlue);
+            const nibble3 = mapValueToPwmElseFloat(outputRed);
+            return createMessageFromNibbles(nibble1, nibble2, nibble3);
+        }
+    }
+
+    const IR_MARK = Math.idiv(6 * 1000000, 38000);
+    const START_STOP_PAUSE = Math.idiv((45 - 6) * 1000000, 38000);
+    const LOW_PAUSE = Math.idiv((16 - 6) * 1000000, 38000);
+    const HIGH_PAUSE = Math.idiv((27 - 6) * 1000000, 38000);
+
+    export class InfraredDevice {
+        private pin: AnalogPin;
+        private waitCorrection: number;
+
+        constructor(pin: AnalogPin, pwmPeriod = 26) {
+            this.pin = pin;
+            pins.analogWritePin(this.pin, 0);
+            pins.analogSetPeriod(this.pin, pwmPeriod);
+
+            {
+                const start = input.runningTimeMicros();
+                const runs = 8;
+                for (let i = 0; i < runs; i++) {
+                    this.transmitBit(1, 1);
+                }
+                const end = input.runningTimeMicros();
+                this.waitCorrection = Math.idiv(end - start - runs * 2, runs * 2);
+            }
+
+            control.waitMicros(2000);
+        }
+
+        public transmitBit(highMicros: number, lowMicros: number): void {
+            pins.analogWritePin(this.pin, 511);
+            control.waitMicros(highMicros);
+            pins.analogWritePin(this.pin, 1);
+            control.waitMicros(lowMicros);
+        }
+
+        public sendMessage(message: number): void {
+            const MAX_LENGTH_MS = 16;
+            const channel = 1 + ((message >> 12) & 0b0011);
+            const ir_mark = IR_MARK - this.waitCorrection;
+            const high_pause = HIGH_PAUSE - this.waitCorrection;
+            const low_pause = LOW_PAUSE - this.waitCorrection;
+            const start_stop_pause = START_STOP_PAUSE - this.waitCorrection;
+
+            for (let sendCount = 0; sendCount < 5; sendCount++) {
+                const MESSAGE_BITS = 16;
+
+                let mask = 1 << (MESSAGE_BITS - 1);
+
+                this.transmitBit(ir_mark, start_stop_pause);
+
+                while (mask > 0) {
+                    if (message & mask) {
+                        this.transmitBit(ir_mark, high_pause);
+                    } else {
+                        this.transmitBit(ir_mark, low_pause);
+                    }
+                    mask >>= 1;
+                }
+
+                this.transmitBit(ir_mark, start_stop_pause);
+
+                if (sendCount == 0 || sendCount == 1) {
+                    basic.pause(5 * MAX_LENGTH_MS);
+                } else {
+                    basic.pause((6 + 2 * channel) * MAX_LENGTH_MS);
+                }
+            }
+        }
+    }
+}
